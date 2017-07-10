aca525ef48c32d18bddfad53b8919c8816c01c81
HarryBon <watermelonha@gmail.com>
2017-07-10 22:33:10 +0800

    增加发现设备的URL路径：api/mission/discover/(?P<mid>[0-9]+)/

--- ipcset/urls.py
+++ ipcset/urls.py
@@ -37,4 +37,5 @@ urlpatterns = [
     url(r'^api/mission/$', views.api_get_add_put_mission),
     url(r'^api/mission/assign/$', views.api_get_waiting_mission),
     url(r'^api/mission/(?P<mid>[0-9]+)/$', views.api_get_add_put_mission_detail),
+    url(r'^api/mission/discover/(?P<mid>[0-9]+)/$', views.api_add_put_discover_mission_detail),
 ]
