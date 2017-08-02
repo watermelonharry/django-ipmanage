07f42c0bb201934f23eb54ea58040a024a57f19c
watermelonharry <bongbongca@foxmail.com>
2017-08-02 13:43:35 +0800

    增加终端注册api， todo：在线终端api，调整时区

--- terminalReg/views.py
+++ terminalReg/views.py
@@ -8,6 +8,7 @@ from django.views.generic.detail import DetailView
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from jsonreformat import *
+from serializers import *
 
 from userManage.models import ApiKeyModel, UserApiModel
 
@@ -15,44 +16,61 @@ from userManage.models import ApiKeyModel, UserApiModel
 # Create your views here.
 
 class TerminalListView(ListView):
-	model = TerminalModel
-	template_name = "terminal_list.html"
+    model = TerminalModel
+    template_name = "terminal_list.html"
 
-	@method_decorator(login_required)
-	def get(self, request):
-		"""
-		show terminals
-		"""
-		terminal_list = TerminalModel.objects.all()
-		return render(request, self.template_name, {'firstTitle': u'终端管理',
-		                                            'firstTitle_content': u'',
-		                                            'terminal_list': terminal_list})
+    @method_decorator(login_required)
+    def get(self, request):
+        """
+        show terminals
+        """
+        terminal_list = TerminalModel.objects.all()
+        return render(request, self.template_name, {'firstTitle': u'终端管理',
+                                                    'firstTitle_content': u'',
+                                                    'terminal_list': terminal_list})
 
 
 class TerminalRegister(DetailView):
-	model = TerminalModel
+    model = TerminalModel
 
-	@csrf_exempt
-	def post(self, request):
-		"""
-		terminal register by POST to this url
-		"""
-		data = JSONParser().parse(request)
-		terminal_ak = data.get('ak', '')
-		api_model = ApiKeyModel.has_record(terminal_ak)
-		user_model = UserApiModel.objects.get(id=api_model.user_id)
+    @csrf_exempt
+    def post(self, request):
+        """
+        terminal register by POST to this url
+        """
+        data = JSONParser().parse(request)
+        terminal_ak = data.get('ak', '')
+        api_model = ApiKeyModel.has_record(terminal_ak)
+        user_model = UserApiModel.objects.get(id=api_model.user_id)
 
-		return SuccessJsonResponse(data={'user': 'user_model.username', 'ak': terminal_ak}, status=200)
+        return SuccessJsonResponse(data={'user': 'user_model.username', 'ak': terminal_ak}, status=200)
+
 
 @csrf_exempt
 def api_temrinal_register_post(request):
-	"""
-	terminal register by POST to this url
-	"""
-	#todo: register with ak,terminal_name,and so on
-	data = JSONParser().parse(request)
-	terminal_ak = data.get('ak', '')
-	api_model = ApiKeyModel.has_record(terminal_ak)
-	user_model = UserApiModel.objects.get(id=api_model.user_id)
+    """
+    terminal register by POST to this url with{ak, terminal_name}
+    """
+    # todo: register with ak,terminal_name,and so on
+    data = JSONParser().parse(request)
+    terminal_ak = data.get('ak', '')
+    api_model = ApiKeyModel.has_record(terminal_ak)
+    if api_model is None:
+        return ErrorJsonResponse(data={'ak':'un verified ak'}, status=400)
 
-	return SuccessJsonResponse(data={'user': user_model.username, 'ak': terminal_ak}, status=200)
+    user_model = UserApiModel.objects.get(id=api_model.user_id)
+    terminal_name = data.get('terminal_name', None)
+    if terminal_name is None:
+        return ErrorJsonResponse(data={'teminal_name':'is null'},status=400)
+
+    try:
+        old_terminal = TerminalModel.objects.get(terminal_name=terminal_name)
+        new_terminal = TerminalModelSerializer(old_terminal,data=data)
+    except Exception as e:
+        new_terminal = TerminalModelSerializer(data=data)
+
+    if new_terminal.is_valid():
+        new_terminal.save()
+        return SuccessJsonResponse(data={'user': user_model.username, 'ak': terminal_ak}, status=200)
+    else:
+        return ErrorJsonResponse(data=new_terminal.errors)
