abcb3895054cda83680f078034e0da47079fc3f8
watermelonharry <bongbongca@foxmail.com>
2017-08-15 13:42:14 +0800

    注册时返回第一个待执行任务

--- terminalReg/models.py
+++ terminalReg/models.py
@@ -7,132 +7,143 @@ TERMINAL_BURN_TIME = 5
 
 
 class TerminalModel(models.Model):
-	"""
-	terminal registration
-	"""
-	TERMINAL_STATUS_LIST = [
-		(1, u'空闲'),
-		(2, u'繁忙'),
-	]
+    """
+    terminal registration
+    """
+    TERMINAL_STATUS_LIST = [
+        (1, u'空闲'),
+        (2, u'繁忙'),
+    ]
 
-	terminal_name = models.CharField(max_length=40, blank=True, unique=True)
-	terminal_status = models.IntegerField(blank=True, null=True, choices=TERMINAL_STATUS_LIST)
-	terminal_type = models.CharField(max_length=20, blank=True, null=True)
+    terminal_name = models.CharField(max_length=40, blank=True, unique=True)
+    terminal_status = models.IntegerField(blank=True, null=True, choices=TERMINAL_STATUS_LIST)
+    terminal_type = models.CharField(max_length=20, blank=True, null=True)
 
-	assigned_mission = models.CharField(max_length=20, blank=True, null=True)
+    assigned_mission = models.CharField(max_length=20, blank=True, null=True)
 
-	available_time = models.IntegerField(blank=True, null=True)
+    available_time = models.IntegerField(blank=True, null=True)
 
-	ak = models.CharField(max_length=50, blank=True, null=True)
-	terminal_addr = models.CharField(max_length=20, blank=True, null=True)
-	terminal_port = models.IntegerField(blank=True, null=True)
+    ak = models.CharField(max_length=50, blank=True, null=True)
+    terminal_addr = models.CharField(max_length=20, blank=True, null=True)
+    terminal_port = models.IntegerField(blank=True, null=True)
 
-	other_info = models.TextField(blank=True, null=True)
+    other_info = models.TextField(blank=True, null=True)
 
-	create_time = models.DateTimeField(auto_now_add=True)
-	edit_time = models.DateTimeField(auto_now=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+    edit_time = models.DateTimeField(auto_now=True)
 
-	class Meta:
-		ordering = ['-edit_time', 'id']
+    class Meta:
+        ordering = ['-edit_time', 'id']
 
-	def __unicode__(self):
-		return self.terminal_name
+    def __unicode__(self):
+        return self.terminal_name
 
-	def is_online(self):
-		"""
-		check the edit time to decide whether the terminal is online
-		5 seconds burn time
-		:return:
-		"""
-		# todo: compare edit time with localtime
-		record = self.edit_time
-		now = datetime.datetime.now(tz=record.tzinfo)
-		time_gap = now - record
-		if time_gap.total_seconds() < TERMINAL_BURN_TIME:
-			return True
-		else:
-			return False
+    def is_online(self):
+        """
+        check the edit time to decide whether the terminal is online
+        5 seconds burn time
+        :return:
+        """
+        # todo: compare edit time with localtime
+        record = self.edit_time
+        now = datetime.datetime.now(tz=record.tzinfo)
+        time_gap = now - record
+        if time_gap.total_seconds() < TERMINAL_BURN_TIME:
+            return True
+        else:
+            return False
 
-	@classmethod
-	def get_online_list(cls):
-		"""
-		get online-terminal list
-		"""
-		all_terminal = cls.objects.all()
-		online_list =[]
-		for terminal in all_terminal:
-			if terminal.is_online():
-				online_list.append(terminal)
-		return online_list
+    @classmethod
+    def get_online_list(cls):
+        """
+        get online-terminal list
+        """
+        all_terminal = cls.objects.all()
+        online_list = []
+        for terminal in all_terminal:
+            if terminal.is_online():
+                online_list.append(terminal)
+        return online_list
 
-	@classmethod
-	def has_terminal(cls, input_name):
-		"""
-		:param input_name:
-		:return:
-		"""
-		try:
-			terminal = cls.objects.get(terminal_name=input_name)
-			return True
-		except Exception as e:
-			return False
-
+    @classmethod
+    def has_terminal(cls, input_name):
+        """
+        :param input_name:
+        :return:
+        """
+        try:
+            terminal = cls.objects.get(terminal_name=input_name)
+            return True
+        except Exception as e:
+            return False
 
 
 class TerminalHistoryModel(models.Model):
-	"""
-	record the mission excuting history
-	"""
+    """
+    record the mission excuting history
+    """
 
-	terminal_id = models.IntegerField(blank=True)
-	mission_id = models.CharField(max_length=40, blank=True)
+    terminal_id = models.IntegerField(blank=True)
+    mission_id = models.CharField(max_length=40, blank=True)
 
-	create_time = models.DateTimeField(auto_now_add=True)
-	edit_time = models.DateTimeField(auto_now=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+    edit_time = models.DateTimeField(auto_now=True)
 
-	def __unicode__(self):
-		return self.terminal_id + self.mission_id
+    def __unicode__(self):
+        return self.terminal_id + self.mission_id
 
-	class Meta:
-		ordering = ['id', 'create_time']
+    class Meta:
+        ordering = ['id', 'create_time']
 
 
 class TerminalWaitingMissionModel(models.Model):
-	"""
-	newly created mission waiting to be fetched by terminals
-	"""
-	mission_status_choice = [
-		(1, u'待分配'),
-		(2, u'已下发')
-	]
-	terminal_name = models.CharField(max_length=40)
-	mission_id = models.CharField(max_length=40)
+    """
+    newly created mission waiting to be fetched by terminals
+    """
+    mission_status_choice = [
+        (1, u'待分配'),
+        (2, u'已下发')
+    ]
+    terminal_name = models.CharField(max_length=40)
+    mission_id = models.CharField(max_length=40)
 
-	# module
-	mission_from = models.CharField(max_length=20)
-	mission_url = models.CharField(max_length=40, blank=True, null=True)
+    # module
+    mission_from = models.CharField(max_length=20)
+    mission_url = models.CharField(max_length=40, blank=True, null=True)
 
-	mission_status = models.IntegerField(choices=mission_status_choice, default=1)
+    mission_status = models.IntegerField(choices=mission_status_choice, default=1)
 
-	create_time = models.DateTimeField(auto_now_add=True)
-	edit_time = models.DateTimeField(auto_now=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+    edit_time = models.DateTimeField(auto_now=True)
 
-	def __unicode__(self):
-		return self.terminal_name + self.mission_id
+    def __unicode__(self):
+        return self.terminal_name + self.mission_id
 
-	class Meta:
-		ordering = ['id', 'create_time']
+    class Meta:
+        ordering = ['id', 'create_time']
 
-	@classmethod
-	def delete_from_queue(cls, mission_id):
-		"""
-		waiting mission deleter
-		:param mission_id:
-		:return:
-		"""
-		try:
-			cls.objects.get(mission_id=mission_id).delete()
-			return True
-		except Exception as e:
-			return False
+    @classmethod
+    def delete_from_queue(cls, mission_id):
+        """
+        waiting mission deleter
+        :param mission_id:
+        :return:
+        """
+        try:
+            cls.objects.get(mission_id=mission_id).delete()
+            return True
+        except Exception as e:
+            return False
 
+    @classmethod
+    def get_mission_by_name(cls, terminal_name):
+        """
+        get waiting mission by input terminal_name
+        :param terminal_name:
+        :return:
+        """
+        try:
+            single_mission = cls.objects.filter(terminal_name=terminal_name)[0]
+            return single_mission
+        except Exception as e:
+            return None
--- terminalReg/views.py
+++ terminalReg/views.py
@@ -16,84 +16,91 @@ from serializers import *
 # Create your views here.
 
 class TerminalListView(ListView):
-	model = TerminalModel
-	template_name = "terminal_list.html"
+    model = TerminalModel
+    template_name = "terminal_list.html"
 
-	@method_decorator(login_required)
-	def get(self, request):
-		"""
-		show terminals
-		"""
-		terminal_list = TerminalModel.objects.all()
-		return render(request, self.template_name, {'firstTitle': u'终端管理',
-		                                            'firstTitle_content': u'',
-		                                            'terminal_list': terminal_list})
+    @method_decorator(login_required)
+    def get(self, request):
+        """
+        show terminals
+        """
+        terminal_list = TerminalModel.objects.all()
+        return render(request, self.template_name, {'firstTitle': u'终端管理',
+                                                    'firstTitle_content': u'',
+                                                    'terminal_list': terminal_list})
 
 
 class TerminalRegister(DetailView):
-	model = TerminalModel
+    model = TerminalModel
 
-	@csrf_exempt
-	def post(self, request):
-		"""
-		terminal register by POST to this url
-		"""
-		data = JSONParser().parse(request)
-		terminal_ak = data.get('ak', '')
-		api_model = ApiKeyModel.has_record(terminal_ak)
-		user_model = UserApiModel.objects.get(id=api_model.user_id)
+    @csrf_exempt
+    def post(self, request):
+        """
+        terminal register by POST to this url
+        """
+        data = JSONParser().parse(request)
+        terminal_ak = data.get('ak', '')
+        api_model = ApiKeyModel.has_record(terminal_ak)
+        user_model = UserApiModel.objects.get(id=api_model.user_id)
 
-		return SuccessJsonResponse(data={'user': 'user_model.username', 'ak': terminal_ak}, status=200)
+        return SuccessJsonResponse(data={'user': 'user_model.username', 'ak': terminal_ak}, status=200)
 
 
 @csrf_exempt
 def api_temrinal_register_post(request):
-	"""
-	terminal register by POST to this url with{ak, terminal_name}
-	"""
-	# todo: register with ak,terminal_name,and so on
-	data = JSONParser().parse(request)
-	terminal_ak = data.get('ak', '')
-	api_model = ApiKeyModel.has_record(terminal_ak)
-	if api_model is None:
-		return ErrorJsonResponse(data={'ak': 'un verified ak'}, status=400)
+    """
+    terminal register by POST to this url with{ak, terminal_name}
+    """
+    # todo: register with ak,terminal_name,and so on
+    data = JSONParser().parse(request)
+    terminal_ak = data.get('ak', '')
+    api_model = ApiKeyModel.has_record(terminal_ak)
+    if api_model is None:
+        return ErrorJsonResponse(data={'ak': 'un verified ak'}, status=400)
 
-	user_model = UserApiModel.objects.get(id=api_model.user_id)
-	terminal_name = data.get('terminal_name', None)
-	if terminal_name is None:
-		return ErrorJsonResponse(data={'teminal_name': 'is null'}, status=400)
+    user_model = UserApiModel.objects.get(id=api_model.user_id)
+    terminal_name = data.get('terminal_name', None)
+    if terminal_name is None:
+        return ErrorJsonResponse(data={'teminal_name': 'is null'}, status=400)
 
-	try:
-		old_terminal = TerminalModel.objects.get(terminal_name=terminal_name)
-		new_terminal = TerminalModelSerializer(old_terminal, data=data)
-	except Exception as e:
-		new_terminal = TerminalModelSerializer(data=data)
+    try:
+        old_terminal = TerminalModel.objects.get(terminal_name=terminal_name)
+        new_terminal = TerminalModelSerializer(old_terminal, data=data)
+    except Exception as e:
+        new_terminal = TerminalModelSerializer(data=data)
 
-	if new_terminal.is_valid():
-		new_terminal.save()
-		return SuccessJsonResponse(data={'user': user_model.username, 'ak': terminal_ak}, status=200)
-	else:
-		return ErrorJsonResponse(data=new_terminal.errors)
+    if new_terminal.is_valid():
+        new_terminal.save()
+
+        try:
+            m_serializer = TerminalWaitingMissionModel.get_mission_by_name(terminal_name=terminal_name)
+            mission_info = m_serializer.data
+        except Exception as e:
+            mission_info = {'mission_id': 'None'}
+
+        return SuccessJsonResponse(data={'user': user_model.username, 'ak': terminal_ak}.update(**mission_info),
+                                   status=200)
+    else:
+        return ErrorJsonResponse(data=new_terminal.errors)
 
 
 class InnerApiBindMissionTerminal(DetailView):
+    # @method_decorator(csrf_exempt)
+    def post(self, request):
+        """
+        api to bind the mission with terminal
+        """
+        format_data = FormatJsonParser(request)
+        if TerminalModel.has_terminal(format_data.get_terminal_name()) is False:
+            return ErrorJsonResponse(data={"terminal_name": "terminal not found"}, status=400)
 
-	# @method_decorator(csrf_exempt)
-	def post(self, request):
-		"""
-		api to bind the mission with terminal
-		"""
-		format_data = FormatJsonParser(request)
-		if TerminalModel.has_terminal(format_data.get_terminal_name()) is False:
-			return ErrorJsonResponse(data={"terminal_name": "terminal not found"}, status=400)
-
-		data = format_data.get_content()
-		new_bind = TerminalWaitingMissionSerializer(data=data)
-		if new_bind.is_valid() is True:
-			new_bind.save()
-			return SuccessJsonResponse(data=data, status=200)
-		else:
-			return ErrorJsonResponse(data=new_bind.errors, status=406)
+        data = format_data.get_content()
+        new_bind = TerminalWaitingMissionSerializer(data=data)
+        if new_bind.is_valid() is True:
+            new_bind.save()
+            return SuccessJsonResponse(data=data, status=200)
+        else:
+            return ErrorJsonResponse(data=new_bind.errors, status=406)
 
 
 @csrf_exempt
@@ -103,15 +110,15 @@ def api_outer_get_online_terminal_list(request):
     :param request:
     :return:
     """
-    if request.method =='POST':
-        data=JSONParser().parse(request)
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
         api_model = ApiKeyModel.has_ak(data)
         if api_model:
             online_list = TerminalModel.get_online_list()
             terminal_data = TerminalModelSerializer(online_list, many=True).data
             return SuccessJsonResponse(data=terminal_data)
         else:
-            return ErrorJsonResponse(data={"ak":"invalid ak"},status=411)
+            return ErrorJsonResponse(data={"ak": "invalid ak"}, status=411)
 
 
 class InnerApiGetOnlineTerminal(ListView):
