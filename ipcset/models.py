ea9fe3b11f93439954c01c02134b18b03946d2a0
HarryBon <watermelonha@gmail.com>
2017-09-05 23:42:16 +0800

    增加start_set_modal的在线终端选择，增加js的refresh_terminal函数,增加任务类的get_by_terminal_name方法

--- ipcset/models.py
+++ ipcset/models.py
@@ -1,184 +1,195 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 
+
 # Create your models here.
 class BaseResolutionTable(models.Model):
-    """
-    分辨率表格
-    """
-    resolution = models.CharField(max_length=20, unique=True)
+	"""
+	分辨率表格
+	"""
+	resolution = models.CharField(max_length=20, unique=True)
 
-    def __unicode__(self):
-        return unicode(self.resolution)
+	def __unicode__(self):
+		return unicode(self.resolution)
 
-    def get_resolution(self):
-        return unicode(self.resolution)
+	def get_resolution(self):
+		return unicode(self.resolution)
 
-    class Meta:
-        ordering = ('id',)
+	class Meta:
+		ordering = ('id',)
+
 
 class BaseBitrateTable(models.Model):
-    """
-    码率表格
-    """
-    bitrate = models.IntegerField(max_length=10,unique=True)
+	"""
+	码率表格
+	"""
+	bitrate = models.IntegerField(max_length=10, unique=True)
 
-    def __unicode__(self):
-        return unicode(self.bitrate)
+	def __unicode__(self):
+		return unicode(self.bitrate)
 
-    def get_bitrate(self):
-        return unicode(self.bitrate)
+	def get_bitrate(self):
+		return unicode(self.bitrate)
+
 
 class BaseFramerateTable(models.Model):
-    """
-    帧率表
-    """
-    framerate = models.IntegerField(max_length=2,unique=True)
+	"""
+	帧率表
+	"""
+	framerate = models.IntegerField(max_length=2, unique=True)
 
-    def __unicode__(self):
-        return unicode(self.framerate)
+	def __unicode__(self):
+		return unicode(self.framerate)
 
-    def get_framerate(self):
-        return unicode(self.framerate)
+	def get_framerate(self):
+		return unicode(self.framerate)
+
 
 # class BaseUserTable(models.Model):
 #     pass
 
 class BaseTypeTable(models.Model):
-    """
-    设备型号表
-    """
-    model_name = models.CharField(max_length=20,unique=True)
-    alias_name = models.CharField(max_length=30, unique=True, blank=True)
-    resolution_set = models.ManyToManyField(BaseResolutionTable)
-    bitrate_set = models.ManyToManyField(BaseBitrateTable)
-    framerate_set = models.ManyToManyField(BaseFramerateTable)
+	"""
+	设备型号表
+	"""
+	model_name = models.CharField(max_length=20, unique=True)
+	alias_name = models.CharField(max_length=30, unique=True, blank=True)
+	resolution_set = models.ManyToManyField(BaseResolutionTable)
+	bitrate_set = models.ManyToManyField(BaseBitrateTable)
+	framerate_set = models.ManyToManyField(BaseFramerateTable)
 
-    min_resolution_set = models.ManyToManyField(BaseResolutionTable, related_name='min_reso_set')
-    min_bitrate_set = models.ManyToManyField(BaseBitrateTable, related_name='min_bitrate_set')
-    min_framerate_set = models.ManyToManyField(BaseFramerateTable, related_name='min_framerate_set')
+	min_resolution_set = models.ManyToManyField(BaseResolutionTable, related_name='min_reso_set')
+	min_bitrate_set = models.ManyToManyField(BaseBitrateTable, related_name='min_bitrate_set')
+	min_framerate_set = models.ManyToManyField(BaseFramerateTable, related_name='min_framerate_set')
 
-    editor_name = models.CharField(max_length=20)
-    create_time = models.DateTimeField(auto_now_add=True)
-    edit_time = models.DateTimeField(auto_now=True)
+	editor_name = models.CharField(max_length=20)
+	create_time = models.DateTimeField(auto_now_add=True)
+	edit_time = models.DateTimeField(auto_now=True)
 
-    def __unicode__(self):
-        return unicode(self.model_name)
+	def __unicode__(self):
+		return unicode(self.model_name)
 
-    def get_content(self):
-        #todo:
-        pass
+	def get_content(self):
+		# todo:
+		pass
 
-    def get_model_name(self):
-        return unicode(self.model_name)
+	def get_model_name(self):
+		return unicode(self.model_name)
 
-    class Meta:
-        ordering = ('id',)
+	class Meta:
+		ordering = ('id',)
 
 
 class VideoSettingTable(models.Model):
-    """
-    设置表
-    """
-    mac_addr = models.CharField(max_length=16)
-    current_ip = models.IPAddressField()
-    type_id = models.ForeignKey(BaseTypeTable)
+	"""
+	设置表
+	"""
+	mac_addr = models.CharField(max_length=16)
+	current_ip = models.IPAddressField()
+	type_id = models.ForeignKey(BaseTypeTable)
 
-    set_resolution = models.CharField(max_length=20)
-    set_bitrate = models.IntegerField(max_length=10)
-    set_framerate = models.CharField(max_length=20)
+	set_resolution = models.CharField(max_length=20)
+	set_bitrate = models.IntegerField(max_length=10)
+	set_framerate = models.CharField(max_length=20)
 
-    set_min_resolution = models.CharField(max_length=20)
-    set_min_bitrate = models.IntegerField(max_length=10)
-    set_min_framerate = models.CharField(max_length=20)
-    #操作类型，1：设置码流参数   2：设置码流参数并同步OSD显示
-    operate_type = models.IntegerField(max_length=2, default=1)
+	set_min_resolution = models.CharField(max_length=20)
+	set_min_bitrate = models.IntegerField(max_length=10)
+	set_min_framerate = models.CharField(max_length=20)
+	# 操作类型，1：设置码流参数   2：设置码流参数并同步OSD显示
+	operate_type = models.IntegerField(max_length=2, default=1)
 
-    editor_name = models.CharField(max_length=20)
-    create_time = models.DateTimeField(auto_now_add=True)
-    edit_time = models.DateTimeField(auto_now=True)
+	editor_name = models.CharField(max_length=20)
+	create_time = models.DateTimeField(auto_now_add=True)
+	edit_time = models.DateTimeField(auto_now=True)
 
-    status = models.IntegerField(max_length=1, default=0)
+	status = models.IntegerField(max_length=1, default=0)
 
-    def __unicode__(self):
-        return unicode(self.mac_addr)
+	def __unicode__(self):
+		return unicode(self.mac_addr)
 
-    def get_download_content(self):
-        return u','.join(map(unicode, [self.edit_time.strftime('%Y-%m-%d %H:%M:%S'),
-                                       self.mac_addr, self.current_ip, self.type_id.model_name,
-                                       self.set_resolution, self.set_bitrate, self.set_framerate,
-                                       self.set_min_resolution, self.set_min_bitrate, self.set_min_framerate,
-                                       self.editor_name])) + u'\n'
+	def get_download_content(self):
+		return u','.join(map(unicode, [self.edit_time.strftime('%Y-%m-%d %H:%M:%S'),
+		                               self.mac_addr, self.current_ip, self.type_id.model_name,
+		                               self.set_resolution, self.set_bitrate, self.set_framerate,
+		                               self.set_min_resolution, self.set_min_bitrate, self.set_min_framerate,
+		                               self.editor_name])) + u'\n'
 
-    class Meta:
-        ordering = ('id',)
+	class Meta:
+		ordering = ('id',)
 
 
 class MissionInfoTable(models.Model):
-    """
-    任务简表
-    """
-    #todo: progress的default = 0会被终止的post(不带progress)影响
-    mission_id = models.CharField(max_length=20, unique=True)
-    total_count = models.IntegerField(max_length=5, blank=True)
-    progress = models.IntegerField(max_length=5, blank=True)
+	"""
+	任务简表
+	"""
+	# todo: progress的default = 0会被终止的post(不带progress)影响
+	mission_id = models.CharField(max_length=20, unique=True)
+	total_count = models.IntegerField(max_length=5, blank=True)
+	progress = models.IntegerField(max_length=5, blank=True)
 
-    remote_id = models.CharField(max_length=20, blank=True)
-    editor_name = models.CharField(max_length=20, blank=True)
-    create_time = models.DateTimeField(auto_now_add=True)
-    edit_time = models.DateTimeField(auto_now=True)
+	remote_id = models.CharField(max_length=20, blank=True)
+	editor_name = models.CharField(max_length=20, blank=True)
+	create_time = models.DateTimeField(auto_now_add=True)
+	edit_time = models.DateTimeField(auto_now=True)
 
-    start_ip = models.IPAddressField(blank=True)
-    # running flag, 1: run,  0: terminate
-    run_status = models.IntegerField(max_length=1, blank=True)
+	start_ip = models.IPAddressField(blank=True)
+	# running flag, 1: run,  0: terminate
+	run_status = models.IntegerField(max_length=1, blank=True)
 
+	##任务类型，1：设置设备  2：发现设备
+	mission_type = models.IntegerField(max_length=2, blank=True)
 
-    ##任务类型，1：设置设备  2：发现设备
-    mission_type = models.IntegerField(max_length=2, blank=True)
+	def __unicode__(self):
+		return unicode(self.mission_id)
 
-    def __unicode__(self):
-        return unicode(self.mission_id)
+	def get_content(self):
+		# todo:
+		pass
 
-    def get_content(self):
-        # todo:
-        pass
+	class Meta:
+		ordering = ('mission_id',)
 
-    class Meta:
-        ordering = ('mission_id',)
+	@classmethod
+	def get_missions_by_terminal(cls, terminal_name):
+		try:
+			terminal_list = cls.objects.filter(remote_id=terminal_name)
+			return terminal_list
+		except Exception as e:
+			return []
+
 
 class MissionDetailTable(models.Model):
-    """
-    任务明细表
-    """
-    mission_id = models.CharField(max_length=20)
+	"""
+	任务明细表
+	"""
+	mission_id = models.CharField(max_length=20)
 
-    mac_addr = models.CharField(max_length=16)
-    current_ip = models.IPAddressField()
-    type_id = models.ForeignKey(BaseTypeTable)
+	mac_addr = models.CharField(max_length=16)
+	current_ip = models.IPAddressField()
+	type_id = models.ForeignKey(BaseTypeTable)
 
-    set_resolution = models.CharField(max_length=20)
-    set_bitrate = models.IntegerField(max_length=10)
-    set_framerate = models.CharField(max_length=20)
+	set_resolution = models.CharField(max_length=20)
+	set_bitrate = models.IntegerField(max_length=10)
+	set_framerate = models.CharField(max_length=20)
 
-    set_min_resolution = models.CharField(max_length=20)
-    set_min_bitrate = models.IntegerField(max_length=10)
-    set_min_framerate = models.CharField(max_length=20)
-    #操作类型，1：设置码流参数   2：设置码流参数并同步OSD显示
-    operate_type = models.IntegerField(max_length=2, default=1)
+	set_min_resolution = models.CharField(max_length=20)
+	set_min_bitrate = models.IntegerField(max_length=10)
+	set_min_framerate = models.CharField(max_length=20)
+	# 操作类型，1：设置码流参数   2：设置码流参数并同步OSD显示
+	operate_type = models.IntegerField(max_length=2, default=1)
 
+	create_time = models.DateTimeField(auto_now_add=True)
+	edit_time = models.DateTimeField(auto_now=True)
 
-    create_time = models.DateTimeField(auto_now_add=True)
-    edit_time = models.DateTimeField(auto_now=True)
+	##0: 等待设置，  3：设置失败  5：设置成功     1：已更新 2：已添加
+	status = models.IntegerField(max_length=1, default=0)
 
-    ##0: 等待设置，  3：设置失败  5：设置成功     1：已更新 2：已添加
-    status = models.IntegerField(max_length=1, default=0)
+	def __unicode__(self):
+		return unicode(self.current_ip)
 
-    def __unicode__(self):
-        return unicode(self.current_ip)
+	def get_content(self):
+		# todo:
+		pass
 
-    def get_content(self):
-        # todo:
-        pass
-
-    class Meta:
-        ordering = ('current_ip',)
+	class Meta:
+		ordering = ('current_ip',)
