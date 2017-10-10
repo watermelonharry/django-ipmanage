7b67f934f201f118448c80bba3855b6d8d8eff0f
watermelonharry <bongbongca@foxmail.com>
2017-10-10 13:42:57 +0800

    iot-models修改

--- iottest/admin.py
+++ iottest/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from models import IotDeviceTable,MissionDetailTable,MissionTable
 # Register your models here.
+
+
+admin.site.register(IotDeviceTable)
+admin.site.register(MissionTable)
+admin.site.register(MissionDetailTable)
--- iottest/models.py
+++ iottest/models.py
@@ -33,14 +33,14 @@ class MissionTable(models.Model):
     mission_id = models.CharField(max_length=40, unique=True, blank=True)
 
     dut_name = models.CharField(max_length=20, blank=True)
-    dut_versiion = models.CharField(max_length=40, blank=True)
+    dut_version = models.CharField(max_length=40, blank=True)
     dut_addr = models.IPAddressField(blank=True)
     dut_type = models.CharField(max_length=10, choices=TYPE_CHOICE, blank=True)
 
     dut_username = models.CharField(max_length=40, blank=True)
     dut_password = models.CharField(max_length=40, null=True, blank=True)
-    dut_cmp_lock = models.IntegerField
 
+    dut_cmp_lock = models.IntegerField(max_length=5, blank=True, null=True)
     other_info = models.TextField(blank=True, null=True)
 
     editor_name = models.CharField(max_length=20, blank=True)
@@ -52,3 +52,43 @@ class MissionTable(models.Model):
 
     class Meta:
         ordering = ("-create_time",)
+
+
+class MissionDetailTable(models.Model):
+    RESULT_CHOICE = (
+        ("PASS", "PASS"),
+        ("FAIL", "FAIL"),
+        ("N/T", "N/T"),
+        ("REF", "REF")
+    )
+
+    mission_id = models.CharField(max_length=40, blank=True)
+    # foreign key to iot device table
+    iot_device = models.ForeignKey(IotDeviceTable)
+
+    can_discover = models.CharField(max_length=10, choices=RESULT_CHOICE, blank=True)
+    discover_comment = models.TextField(null=True, blank=True)
+
+    can_add = models.CharField(max_length=10, choices=RESULT_CHOICE, blank=True)
+    add_comment = models.TextField(null=True, blank=True)
+
+    can_preview = models.CharField(max_length=10, choices=RESULT_CHOICE, blank=True)
+    preview_comment = models.TextField(null=True, blank=True)
+
+    can_calculate = models.CharField(max_length=10, choices=RESULT_CHOICE, blank=True)
+    calculate_comment = models.TextField(null=True, blank=True)
+
+    can_delete = models.CharField(max_length=10, choices=RESULT_CHOICE, blank=True)
+    delete_comment = models.TextField(null=True, blank=True)
+
+    other_info = models.TextField(blank=True, null=True)
+    dut_cmp_lock = models.IntegerField(max_length=5, blank=True, null=True)
+
+    create_time = models.DateTimeField(auto_now_add=True)
+    edit_time = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return "{0}-{1}".format(self.mission_id, self.iot_device.device_name)
+
+    class Meta:
+        ordering = ("id",)
