5afb036711c76cf64536d964c269ce39b09696e5
HarryBon <watermelonha@gmail.com>
2018-03-12 22:15:58 +0800

    删除dnstest模块

--- dnstest/forms.py
+++
@@ -1,8 +0,0 @@
-from models import CnTestSubscribeList
-from django.forms import ModelForm
-
-
-class CnTestSubscribeForm(ModelForm):
-    class Meta:
-        model = CnTestSubscribeList
-        exclude = ['subscribeTime', 'testStatus', 'testLog']
