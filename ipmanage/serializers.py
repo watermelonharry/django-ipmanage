989922553eeaf6a78205651a5dc5a6dde423a0ef
HarryBon <watermelonha@gmail.com>
2018-03-14 23:34:19 +0800

    增加config_table的put和post序列化器

--- ipmanage/serializers.py
+++ ipmanage/serializers.py
@@ -7,28 +7,32 @@ from models import *
 class ConfigGetterSerializer(serializers.ModelSerializer):
     class Meta:
         model = ConfigTable
-        fields = ("id", "config_name", "config_info", "config_count", "config_bind_topo", "user_name")
+        fields = ("id", "config_name", "config_info", "config_count", "config_bind_topo", "user_name", "editor_name")
+
+
+class ConfigPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ConfigTable
+        fields = ("config_name", "config_info", "config_count", "config_bind_topo", "user_name", "editor_name")
+
+
+class ConfigPutSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ConfigTable
+        fields = ("id", "config_name", "config_info", "config_count", "config_bind_topo", "user_name", "editor_name")
 
 
 class ConfigDetailGetterSerializer(serializers.ModelSerializer):
     class Meta:
         model = ConfigDetailTable
         fields = (
-        "id", "mac_addr", "ori_ip", "set_ip", "ori_password", "set_password", "device_type", "device_firmware",
-        "device_osd", "other_info", "creator_name", "edit_time")
+            "id", "mac_addr", "ori_ip", "set_ip", "ori_password", "set_password", "device_type", "device_firmware",
+            "device_osd", "other_info", "creator_name", "edit_time")
 
 
-# class IpMissionSerializer(serializers.ModelSerializer):
-# class Meta:
-#         model = IpMissionTable
-#         fields = ('id', 'mission_id', 'mission_type', 'start_ip', 'total_count',
-#                   'progress', 'run_status', 'user_name',
-#                   'editor_name', 'edit_time')
-#
-#
-# class IpMissionDetailSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = IpMissionDetailTable
-#         fields = ('id', 'mission_id', 'mac_addr', 'ori_ip', 'set_ip', 'other_info',
-#                   "ori_password", "set_password",
-#                   'status', 'edit_time', 'editor_name', 'user_name')
+class ConfigDetailPostSerializer(serializers.ModelSerializer):
+    config_table_key = serializers.PrimaryKeyRelatedField(blank=True)
+
+    class Meta:
+        model = ConfigDetailTable
+        exclude = ("edit_time", "create_time", "lock")
